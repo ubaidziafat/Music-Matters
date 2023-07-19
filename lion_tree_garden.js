732eeb68e8e83098b830ac737ffa842c43c84d69
ubaidziafat <136702810+ubaidziafat@users.noreply.github.com>
2023-07-19 14:30:32 +0700

    Fix a bug

---
+++ lion_tree_garden.js
@@ -0,0 +1,118 @@
+//Basic Information
+
+let bandName = 'Music Matters';
+let genre = 'Rock';
+let bandMembers = ['John', 'Alice', 'Paul', 'Sam'];
+
+//Concert Scheduling
+
+let concertDates = ['16 August', '23 August', '30 August', '6 September'];
+let concertLocations = ['Los Angeles', 'New York', 'Chicago', 'Dallas'];
+
+//Tour Management
+
+let tourManager = 'John Smith';
+let tourBusCompany = 'Luxury Travel';
+let tourSchedule = {
+  LA: '16 August-22 August',
+  NY: '23 August-29 August',
+  CHI: '30 August-5 September',
+  DAL: '6 September-12 September'
+};
+
+//Rehearsal Planning
+
+let rehearsalLength = 2;
+let rehearsalSchedule = {
+  LA: '14 August-15 August',
+  NY: '21 August-22 August',
+  CHI: '28 August-29 August',
+  DAL: '4 September-5 September'
+};
+
+//Instrument List
+
+let guitarAmp = 'Fender';
+let drums = 'Ludwig';
+let bass = 'Rickenbacker';
+let keyboard = 'Yamaha';
+
+//Setlist
+
+let setlist = ['Greatest Show', 'Hold On', 'Rock Around the Clock', 'The Nights', 'Don’t Stop Believing', 'Bohemian Rhapsody', 'All the Small Things'];
+
+//Ticket Sales
+
+let ticketSalesStartTime = '08 August';
+let ticketSalesEndTime = '15 August';
+let ticketPrice = '$40';
+
+//Merchandise
+
+let merchandiseItems = ['t-shirts', 'hoodies', 'hats', 'posters'];
+let merchandisePrices = {
+  tshirt: '$20',
+  hoodie: '$35',
+  hat: '$15',
+  poster: '$10'
+};
+
+//Social Media
+
+let socialMediaLinks = {
+  Facebook: 'facebook.com/musicmatters',
+  Instagram: 'instagram.com/musicmatters',
+  Twitter: 'twitter.com/musicmatters',
+  YouTube: 'youtube.com/musicmatters'
+};
+
+//Music Samples
+
+let musicSamplesLinks = {
+  'Greatest Show': 'musicmatters.com/greatestshow',
+  'Hold On': 'musicmatters.com/holdon',
+  'Rock Around the Clock': 'musicmatters.com/rockaroundtheclock',
+  'The Nights': 'musicmatters.com/thenights',
+  'Don’t Stop Believing': 'musicmatters.com/dontstopbelieving',
+  'Bohemian Rhapsody': 'musicmatters.com/bohemianrhapsody',
+  'All the Small Things': 'musicmatters.com/allthesmallthings'
+};
+
+//Interviews
+
+let interviewSchedule = {
+  '16 August': 'Los Angeles Times',
+  '22 August': 'New York Times',
+  '29 August': 'Chicago Tribune',
+  '5 September': 'Dallas Morning News'
+};
+
+//Reviews
+
+let reviewReleaseDates = ['17 August', '24 August', '31 August', '7 September'];
+let reviewSources = ['Rolling Stone', 'Pitchfork', 'New York Times', 'Billboard'];
+
+//Live Stream
+
+let liveStreamUrl = 'musicmatters.com/livestream';
+let liveStreamDate = '13 September';
+
+//Live Recording
+
+let liveRecordingDate = '14 September';
+let liveRecordingLocation = 'Dallas';
+
+//Photo/Video Shoot
+
+let photoVideoShootDates = ['15 August', '22 August', '29 August', '6 September'];
+let photoVideoShootLocations = ['Los Angeles', 'New York', 'Chicago', 'Dallas'];
+
+//Promotional Materials
+
+let pressReleaseDates = ['10 August', '17 August', '24 August', '31 August'];
+let pressReleaseOutlets = ['Los Angeles Times', 'New York Times', 'Chicago Tribune', 'Dallas Morning News'];
+
+//Backstage Passes
+
+let backstagePassRequirements = 'Backstage passes are available to select individuals only. To request a backstage pass, please email info@musicmatters.com.';
+let backstagePassReleaseDates = ['12 August', '19 August', '26 August', '2 September'];
